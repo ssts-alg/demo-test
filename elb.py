fc1448e82bace1326d8565f388071973e220925b
suresh <telugu_sureshbabu@intuit.com>
2020-06-02 23:19:33 +0530

    test

--- elb.py
+++ elb.py
@@ -1,17 +1,19 @@
 #! /usr/local/bin/python3
-import botocore
+
 import os
 import boto3
 import sys
 region=input("Enter Region: ")
-
 ec2_tag_name=input("Enter EC2 Tag name to Deregister from Load Balancer: ")
 elb_name=input("Enter target ELB : ")
+
 os.environ["AWS_SHARED_CREDENTIALS_FILE"] = "./creds"
 os.environ["AWS_DEFAULT_REGION"] = region
+
 client = boto3.client('ec2')
 elb_client = boto3.client('elb')
 sts = boto3.client('sts')
+
 try:
     sts.get_caller_identity()
     print("Credentials are valid.")
@@ -19,18 +21,19 @@ except:
     print("Credentials invalid.")
 
 elb_list=[]
+instance_list=[]
+
 response1 = elb_client.describe_load_balancers()
 a=response1['LoadBalancerDescriptions']
+
 for z in a:
     elb_list.append(z['LoadBalancerName'])
+
 if elb_name not in elb_list:
     print(f"{elb_name} ELB not Foung in this {region}")
     sys.exit(0)
 else:
     print(f"Trying to Deregister Instances from {elb_name} ELB")
-
-
-instance_list=[]
 
 response = client.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': [ec2_tag_name]}])
 for x in response['Reservations']:
