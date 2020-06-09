db27c3122f767ca51fa779f5ca3ca8a96374ab14
suresh <telugu_sureshbabu@intuit.com>
2020-06-09 13:11:24 +0530

    test

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-access_key="HSGCJHSAGCJHSAGVHJASGV"
-secret_access_key="DGCAHJCAHJCAHJFCJHASFCHJAFSCJCJ"
-region="us-east-1"
-ec2_tag_name="Jenkins"
-availablity_zone="us-east-1e"
-elb_name="elb"
+access_key="JHDVGJDSGVJDSGVJDSVK"
+secret_access_key="HDVHJVJHSVJHVJACJADKDAKKADKK"
+regions=["us-east-1","us-west-2"]
+ec2_tag_name=["APACHE","JFROG","TOMCAT"]
+availablity_zone=["us-east-1e","us-west-2a"]
+elb_names=["test1","test2"]
--- elb.py
+++ elb.py
@@ -5,18 +5,9 @@ import boto3
 import sys
 from config import *
 
-# Declaring Clients
-client = boto3.client('ec2',aws_access_key_id=access_key,aws_secret_access_key=secret_access_key,region_name=region)
-elb_client = boto3.client('elb',aws_access_key_id=access_key,aws_secret_access_key=secret_access_key,region_name=region)
-sts = boto3.client('sts',aws_access_key_id=access_key,aws_secret_access_key=secret_access_key,region_name=region)
-
-#Region Validation
-regions = [region['RegionName'] for region in client.describe_regions()['Regions']]
-if region not in regions:
-    print(f"{region}No Such Region Found.")
-    sys.exit(2)
-
+default_region="us-west-2"
 #Credentials Validation
+sts = boto3.client('sts',aws_access_key_id=access_key,aws_secret_access_key=secret_access_key,region_name=default_region)
 try:
     sts.get_caller_identity()
     print("Credentials are valid.")
@@ -24,39 +15,72 @@ except:
     print("Credentials invalid. Please Provide Valid Credentials.")
     sys.exit(1)
 
+#Region Validation
+client = boto3.client('ec2',aws_access_key_id=access_key,aws_secret_access_key=secret_access_key,region_name=default_region)
+regions_list = [region['RegionName'] for region in client.describe_regions()['Regions']]
+for region in regions:
+    if region not in regions_list:
+        print(f"{region} No Such Region Found.")
+        sys.exit(2)
+
+#Checks
+if len(regions)==0:
+    print("Please Enter Atleast One region in Config file.")
+    sys.exit(4)
+if len(ec2_tag_name)==0:
+    print("Please Enter Atleast One EC2 Tag Name in Config file.")
+    sys.exit(4)
+if len(availablity_zone)==0:
+    print("Please Enter Atleast One AvailabilityZone in Config file.")
+    sys.exit(4)
+if len(elb_names)==0:
+    print("Please Enter Atleast One ELB Name in Config file.")
+    sys.exit(4)
+
+
 elb_list=[]
 instance_list=[]
-
 # Fetching all available ELBs names from this account.
-elb_response = elb_client.describe_load_balancers()
-a=elb_response['LoadBalancerDescriptions']
-for z in a:
-    elb_list.append(z['LoadBalancerName'])
+for region in regions:
+    elb_client = boto3.client('elb',aws_access_key_id=access_key,aws_secret_access_key=secret_access_key,region_name=region)
+    elb_response = elb_client.describe_load_balancers()
+    a=elb_response['LoadBalancerDescriptions']
+    for z in a:
+        elb_list.append(z['LoadBalancerName'])
 
 # ELB name Validation
-if elb_name not in elb_list:
-    print(f"{elb_name} ELB not Found in this {region}")
-    sys.exit(0)
-else:
-    print(f"Trying to Deregister Instances from {elb_name} ELB")
-elb_response1=elb_client.describe_load_balancers(LoadBalancerNames=[elb_name])
+for elb_name in elb_names:
+    if elb_name not in elb_list:
+        print(f"{elb_name} ELB not Found in following regions {regions}")
+        sys.exit(0)
 
-insta=elb_response1['LoadBalancerDescriptions'][0]['Instances']
-for s in insta:
-    instance_list.append(s['InstanceId'])
+print("Trying to Deregister Instances")
+
 deregistered_instances=[]
-response = client.describe_instances(Filters=[{'Name': 'instance-id','Values': instance_list}])
-for d in response['Reservations']:
-    for z in d['Instances']:
-        if z['Placement']['AvailabilityZone']==availablity_zone and z['Tags'][0]['Value']==ec2_tag_name:
-            deregistered_instances.append(z['InstanceId'])
-            # Deregistering Instances from ELB
-            elb_response = elb_client.deregister_instances_from_load_balancer(
-                           LoadBalancerName=elb_name,
-                           Instances=[
-                                    {
-                                        'InstanceId': z['InstanceId']
-                                    }
-                                    ]
-                    )
-print(f"{deregistered_instances} are Deregistered Successfully")
+for region in regions:
+    new_list=[]
+    elb_client = boto3.client('elb',aws_access_key_id=access_key,aws_secret_access_key=secret_access_key,region_name=region)
+    elb_response=elb_client.describe_load_balancers()
+    a=elb_response['LoadBalancerDescriptions']
+    for z in a:
+        if z['LoadBalancerName'] in elb_names:
+            new_list.append(z['LoadBalancerName'])
+            elb_response1=elb_client.describe_load_balancers(LoadBalancerNames=new_list)
+            insta=elb_response1['LoadBalancerDescriptions']
+            for f in insta:
+                for t in f['Instances']:
+                    instance_list.append(t['InstanceId'])
+                    client = boto3.client('ec2',aws_access_key_id=access_key,aws_secret_access_key=secret_access_key,region_name=region)
+                    response = client.describe_instances(Filters=[{'Name': 'instance-id','Values': instance_list}])
+                    for d in response['Reservations']:
+                        for z in d['Instances']:
+                            tag=z['Tags'][0]['Value']
+                            if z['Placement']['AvailabilityZone'] in availablity_zone and z['Tags'][0]['Value'] in ec2_tag_name:
+                                deregistered_instances.append(z['InstanceId'])
+                                # Deregistering Instances from ELB
+                                for elb in new_list:
+                                    elb_response = elb_client.deregister_instances_from_load_balancer(LoadBalancerName=elb,Instances=[{'InstanceId': z['InstanceId']}])
+if len(list(dict.fromkeys(deregistered_instances)))==0 :
+    print("Nothing to Deregister")
+else:
+    print(f"{list(dict.fromkeys(deregistered_instances))}  removed from ELB")
